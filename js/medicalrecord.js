9867d4dccde6016cbbd0bdc85f10a4264c9aa27e
DonajiNavarro15 <ada_hack49@hackademy.mx>
2025-01-08 00:26:04 -0600

    fix: adecuacion de permisos

--- js/medicalrecord.js
+++ js/medicalrecord.js
@@ -3,7 +3,7 @@ function protegerRutaPaciente() {
     const token = localStorage.getItem("token");
     const role = localStorage.getItem("role");
 
-    if (!token || role !== "admin") {
+    if (!token || role !== "medico") {
         alert("Acceso denegado. No cuentas con los permisos necesarios.");
         window.location.href = "login.html";
     }
